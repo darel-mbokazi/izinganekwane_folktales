4a40dad52f054e876cb904932199fc2e7ef3470f
Siyabonga Mbokazi <darelmbokazi@gmail.com>
2025-03-27 12:09:02 +0200

    fixing comments and replies

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  'https://izinganekwane-folktales-backend.vercel.app'
+  'https://izinganekwane-folktales-backend.vercel.app/api'
 
 export { API_URL };
